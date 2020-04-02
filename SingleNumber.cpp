d5ad53c236bcd1d9406f9c96a12f0f237f35aa09
R@jeev <singhrajiv774@gmail.com>
2020-04-02 14:56:58 +0530

    class name and comments added

--- SingleNumber.cpp
+++ SingleNumber.cpp
@@ -1,9 +1,9 @@
-class Solution {
+class singleNumberClass {
 public:
     int singleNumber(vector<int>& ar) {
         int res = ar[0]; 
         for (int i = 1; i < ar.size(); i++) 
-            res = res ^ ar[i]; 
+            res = res ^ ar[i];                     # Apply XOR operation
         return res;
     }
 };
