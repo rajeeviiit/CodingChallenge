183824aa6c4e67f6c32c215502c5d27a17ab00e1
R@jeev <singhrajiv774@gmail.com>
2020-04-02 16:33:08 +0530

    Comment added

--- HappyNumber.cpp
+++ HappyNumber.cpp
@@ -1,8 +1,8 @@
-
-
+Time : O(log(n))
+Space : O(1)
 class HappyNumber {
 public:
-  
+   # return the squared sum of digits of given number n.
     int numSquareSum(int n)        
     { 
         int squareSum = 0; 
@@ -13,17 +13,19 @@ public:
         } 
         return squareSum; 
     } 
-
+   # return true if number is happy number
     bool isHappy(int n) {            # 
         int slow, fast; 
         slow = fast = n; 
         do
         { 
+          # move slow by one iteration
             slow = numSquareSum(slow); 
+          # move fast by two iteration
             fast = numSquareSum(numSquareSum(fast)); 
         } 
         while (slow != fast); 
-
+        # if both number meet at 1 then return true
         return (slow == 1);
 
         }
